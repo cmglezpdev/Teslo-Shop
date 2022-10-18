4161d1baf96fd89e59b9837ccbec0f06dd251a3b
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-10-18 10:06:15 -0400

    style: add some configurations

--- themes/light-theme.ts
+++ themes/light-theme.ts
@@ -10,6 +10,9 @@ export const lightTheme = createTheme({
     },
     secondary: {
       main: '#3A64D8'
+    },
+    info: {
+      main: '#fff'
     }
   },
   components: {
@@ -55,11 +58,10 @@ export const lightTheme = createTheme({
         variant: 'contained',
         size: 'small',
         disableElevation: true,
+        color: 'info',
       },
       styleOverrides: {
         root: {
-          backgroundColor: 'white',
-          color: 'black',
           textTransform: 'none',
           boxShadow: 'none',
           borderRadius: 10,
