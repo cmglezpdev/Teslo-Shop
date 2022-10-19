c4f9d4f0d1fe7d9d13cbc5f57bd1d9184cad9ad1
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-10-19 12:15:00 -0400

    feat: add page for the address of the order

---
+++ pages/checkout/address.tsx
@@ -0,0 +1,57 @@
+import { Typography, Grid, TextField, FormControl, MenuItem, InputLabel, Select, Box, Button } from '@mui/material';
+import { ShopLayout } from "../../layouts"
+
+
+const AddressPage = () => {
+    return (
+        <ShopLayout title='Address' pageDescription='Confirm destination address'>
+            <Typography variant='h1' component='h1'>Address</Typography>
+        
+            <Grid container spacing={2} sx={{mt:2}}>
+                <Grid item xs={12} sm={6}>
+                    <TextField label='Name' variant='outlined' fullWidth />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <TextField label='Last Name' variant='outlined' fullWidth />
+                </Grid>
+
+                <Grid item xs={12} sm={6}>
+                    <TextField label='Address' variant='outlined' fullWidth />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <TextField label='Address 2 (optional)' variant='outlined' fullWidth />
+                </Grid>
+
+                <Grid item xs={12} sm={6}>
+                    <TextField label='Post Code' variant='outlined' fullWidth />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <TextField label='City' variant='outlined' fullWidth />
+                </Grid>
+
+                <Grid item xs={12} sm={6}>
+                    <FormControl fullWidth>
+                        {/* <InputLabel>Country</InputLabel> */}
+                        <Select variant='outlined'>
+                            <MenuItem value={1}>Costa Rica</MenuItem>
+                            <MenuItem value={2}>Honduras</MenuItem>
+                            <MenuItem value={3}>El Salvador</MenuItem>
+                            <MenuItem value={4}>Mexico</MenuItem>
+                        </Select>
+                    </FormControl>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <TextField label='Phone' variant='outlined' fullWidth />
+                </Grid>
+            </Grid>
+
+            <Box sx={{mt:5}} display='flex' justifyContent='center'>
+                <Button color='secondary' className='circular-btn' sx={{px: 5}}>
+                    Complete Order
+                </Button>
+            </Box>
+        </ShopLayout>   
+    )
+}
+
+export default AddressPage
