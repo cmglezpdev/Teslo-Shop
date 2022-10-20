de003845fef24622cbd5586e7170015bb45ba9dd
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-10-20 00:03:23 -0400

    feat: add schrema of the products

--- interfaces/products.ts
+++ interfaces/products.ts
@@ -10,6 +10,7 @@ export interface IProduct {
     title: string;
     type: IType;
     gender: 'men'|'women'|'kid'|'unisex'
+    // TODO: agregar createdAt and updatedAt
 }
 
 export type ISize = 'XS'|'S'|'M'|'L'|'XL'|'XXL'|'XXXL';
---
+++ models/Product.ts
@@ -0,0 +1,41 @@
+import mongoose, { Model, model, Schema } from 'mongoose';
+import { IProduct } from '../interfaces';
+
+const productSchrema = new Schema({
+    desciption: { type: String, require: true },
+    images: [{ type: String }],
+    inStock: { type: Number, require: true, default: 0 },
+    price: { type: Number, require: true, default: 0 },
+    sizes: [{
+        type: String,
+        enum: {
+            values: ['XS', 'S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+            message: '{VALUE} is not a valid size'
+        }
+    }],
+    slug: { type: String, require: true, unique: true },
+    tags: [{ type: String }],
+    title: { type: String, require: true },
+    type: {
+        type: String,
+        enum: {
+            values: ['shirts', 'pants', 'hoodies', 'hats'],
+            message: '{VALUE} is not a valid type'
+        }
+    },
+    gender: {
+        type: String,
+        enum: {
+            values: ['men', 'women', 'kid', 'unisex'],
+            message: '{VALUE} is not a gender type'
+        }
+    },
+},{
+    timestamps: true
+})
+
+// TODO: Crear indice de Mongo
+
+const Product:Model<IProduct> = mongoose.models.Product || model('Product', productSchrema);
+
+export default Product;
