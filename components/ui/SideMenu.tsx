a5c5cc5e8afb16e5b3bc174fa2c1b4f6327a57c0
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-10-20 21:23:39 -0400

    feat: add can open the side menu

--- components/ui/SideMenu.tsx
+++ components/ui/SideMenu.tsx
@@ -1,112 +1,118 @@
+import { useContext } from 'react';
 import { Box, Divider, Drawer, IconButton, Input, InputAdornment, List, ListItem, ListItemIcon, ListItemText, ListSubheader } from "@mui/material"
 import { AccountCircleOutlined, AdminPanelSettings, CategoryOutlined, ConfirmationNumberOutlined, EscalatorWarningOutlined, FemaleOutlined, LoginOutlined, MaleOutlined, SearchOutlined, VpnKeyOutlined } from "@mui/icons-material"
+import { UIContext } from '../../context';
 
 
 export const SideMenu = () => {
-  return (
-    <Drawer
-        open={ false }
-        anchor='right'
-        sx={{ backdropFilter: 'blur(4px)', transition: 'all 0.5s ease-out' }}
-    >
-        <Box sx={{ width: 250, paddingTop: 5 }}>
-            
-            <List>
 
-                <ListItem>
-                    <Input
-                        type='text'
-                        placeholder="Search..."
-                        endAdornment={
-                            <InputAdornment position="end">
-                                <IconButton
-                                    aria-label="toggle password visibility"
-                                >
-                                 <SearchOutlined />
-                                </IconButton>
-                            </InputAdornment>
-                        }
-                    />
-                </ListItem>
+    const { toggleSideMenu, isMenuOpen } = useContext(UIContext);
 
-                <ListItem button>
-                    <ListItemIcon>
-                        <AccountCircleOutlined/>
-                    </ListItemIcon>
-                    <ListItemText primary='Profile' />
-                </ListItem>
+    return (
+        <Drawer
+            open={ isMenuOpen }
+            onClose={() => toggleSideMenu()}
+            anchor='right'
+            sx={{ backdropFilter: 'blur(4px)', transition: 'all 0.5s ease-out' }}
+        >
+            <Box sx={{ width: 250, paddingTop: 5 }}>
+                
+                <List>
 
-                <ListItem button>
-                    <ListItemIcon>
-                        <ConfirmationNumberOutlined/>
-                    </ListItemIcon>
-                    <ListItemText primary='My orders' />
-                </ListItem>
+                    <ListItem>
+                        <Input
+                            type='text'
+                            placeholder="Search..."
+                            endAdornment={
+                                <InputAdornment position="end">
+                                    <IconButton
+                                        aria-label="toggle password visibility"
+                                    >
+                                    <SearchOutlined />
+                                    </IconButton>
+                                </InputAdornment>
+                            }
+                        />
+                    </ListItem>
 
+                    <ListItem button>
+                        <ListItemIcon>
+                            <AccountCircleOutlined/>
+                        </ListItemIcon>
+                        <ListItemText primary='Profile' />
+                    </ListItem>
 
-                <ListItem button sx={{ display: { xs: '', sm: 'none' } }}>
-                    <ListItemIcon>
-                        <MaleOutlined/>
-                    </ListItemIcon>
-                    <ListItemText primary='Men' />
-                </ListItem>
-
-                <ListItem button sx={{ display: { xs: '', sm: 'none' } }}>
-                    <ListItemIcon>
-                        <FemaleOutlined/>
-                    </ListItemIcon>
-                    <ListItemText primary='Women' />
-                </ListItem>
-
-                <ListItem button sx={{ display: { xs: '', sm: 'none' } }}>
-                    <ListItemIcon>
-                        <EscalatorWarningOutlined/>
-                    </ListItemIcon>
-                    <ListItemText primary='Kids' />
-                </ListItem>
+                    <ListItem button>
+                        <ListItemIcon>
+                            <ConfirmationNumberOutlined/>
+                        </ListItemIcon>
+                        <ListItemText primary='My orders' />
+                    </ListItem>
 
 
-                <ListItem button>
-                    <ListItemIcon>
-                        <VpnKeyOutlined/>
-                    </ListItemIcon>
-                    <ListItemText primary='Enter' />
-                </ListItem>
+                    <ListItem button sx={{ display: { xs: '', sm: 'none' } }}>
+                        <ListItemIcon>
+                            <MaleOutlined/>
+                        </ListItemIcon>
+                        <ListItemText primary='Men' />
+                    </ListItem>
 
-                <ListItem button>
-                    <ListItemIcon>
-                        <LoginOutlined/>
-                    </ListItemIcon>
-                    <ListItemText primary='Exit' />
-                </ListItem>
+                    <ListItem button sx={{ display: { xs: '', sm: 'none' } }}>
+                        <ListItemIcon>
+                            <FemaleOutlined/>
+                        </ListItemIcon>
+                        <ListItemText primary='Women' />
+                    </ListItem>
+
+                    <ListItem button sx={{ display: { xs: '', sm: 'none' } }}>
+                        <ListItemIcon>
+                            <EscalatorWarningOutlined/>
+                        </ListItemIcon>
+                        <ListItemText primary='Kids' />
+                    </ListItem>
 
 
-                {/* Admin */}
-                <Divider />
-                <ListSubheader>Admin Panel</ListSubheader>
+                    <ListItem button>
+                        <ListItemIcon>
+                            <VpnKeyOutlined/>
+                        </ListItemIcon>
+                        <ListItemText primary='Enter' />
+                    </ListItem>
 
-                <ListItem button>
-                    <ListItemIcon>
-                        <CategoryOutlined/>
-                    </ListItemIcon>
-                    <ListItemText primary='Products' />
-                </ListItem>
+                    <ListItem button>
+                        <ListItemIcon>
+                            <LoginOutlined/>
+                        </ListItemIcon>
+                        <ListItemText primary='Exit' />
+                    </ListItem>
 
-                <ListItem button>
-                    <ListItemIcon>
-                        <ConfirmationNumberOutlined/>
-                    </ListItemIcon>
-                    <ListItemText primary='Orders' />
-                </ListItem>
 
-                <ListItem button>
-                    <ListItemIcon>
-                        <AdminPanelSettings/>
-                    </ListItemIcon>
-                    <ListItemText primary='Users' />
-                </ListItem>
-            </List>
-        </Box>
-    </Drawer>
-  )
+                    {/* Admin */}
+                    <Divider />
+                    <ListSubheader>Admin Panel</ListSubheader>
+
+                    <ListItem button>
+                        <ListItemIcon>
+                            <CategoryOutlined/>
+                        </ListItemIcon>
+                        <ListItemText primary='Products' />
+                    </ListItem>
+
+                    <ListItem button>
+                        <ListItemIcon>
+                            <ConfirmationNumberOutlined/>
+                        </ListItemIcon>
+                        <ListItemText primary='Orders' />
+                    </ListItem>
+
+                    <ListItem button>
+                        <ListItemIcon>
+                            <AdminPanelSettings/>
+                        </ListItemIcon>
+                        <ListItemText primary='Users' />
+                    </ListItem>
+                </List>
+            </Box>
+        </Drawer>
+    )
 }
