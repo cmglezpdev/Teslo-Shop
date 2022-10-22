e082c3e27f69cd63332c650aa850b12e91fef030
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-10-21 20:08:26 -0400

    feat: add function to get the products by slug

---
+++ database/db-products.ts
@@ -0,0 +1,17 @@
+import { IProduct } from '../interfaces';
+import { Product } from '../models';
+import { db } from './';
+
+
+export const getProductBySlug = async ( slug: string ):Promise<IProduct | null> => {
+
+    await db.connect();
+    const product = await Product.findOne({ slug }).lean();
+    await db.disconnect();
+    
+    if( !product ) {
+        return null;
+    }
+
+    return JSON.parse( JSON.stringify( product ) );
+}
--- database/index.ts
+++ database/index.ts
@@ -1,4 +1,5 @@
 export * as db from './db'
+export * as dbProducts from './db-products'
 export * from './constants'
 
 // Database of test
