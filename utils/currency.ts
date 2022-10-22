85f13c5a19b2244fa5ea213bd9ac5d7247636b89
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-10-22 01:49:52 -0400

    feat: add function to format a number to a currency

---
+++ utils/currency.ts
@@ -0,0 +1,12 @@
+
+export const format = (value: number) => {
+
+    const formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+    })
+
+    return formatter.format(value);
+}
