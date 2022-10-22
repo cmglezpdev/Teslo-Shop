1327012f1e2625b795f99cdde4e634e86fa7b2ac
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-10-22 13:30:31 -0400

    feat: add method to validate a JWT

--- utils/jwt.ts
+++ utils/jwt.ts
@@ -16,3 +16,24 @@ export const signToken = (_id: string, email: string) => {
         { expiresIn: '30d' }
     )
 }
+
+
+export const isValidToken = ( token:string ):Promise<string> => {
+    if( !process.env.JWT_SECREET_SEED ) {
+        throw new Error('There are not jwt seed - See enviroment variables')
+    }
+
+    return new Promise((resolve, reject) => {
+        try {
+            jwt.verify(token, process.env.JWT_SECREET_SEED!, (error, payload) => {
+                if( error ) 
+                    return reject('JWT is not valid');
+                    
+                    const { _id } = payload as {_id: string};
+                    resolve(_id);
+                })
+        } catch (error) {       
+            reject('JWT is not valid');
+        }
+    })
+}
