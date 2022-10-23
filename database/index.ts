c14361870c24f3e463daba3e97fc8b013a33abf0
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-10-23 12:53:23 -0400

    feat: add database with information of all contries

--- database/index.ts
+++ database/index.ts
@@ -1,6 +1,7 @@
 export * as db from './db'
 export * as dbProducts from './db-products'
 export * from './constants'
+export * from './countries'
 
 // Database of test
 export * as seedDatabase from './seed-data'
