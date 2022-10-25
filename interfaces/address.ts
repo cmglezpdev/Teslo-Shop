67d4b3a505e10eeefdde29a66cd02c1e02ea7126
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-10-25 10:02:44 -0400

    feat: add address interface

---
+++ interfaces/address.ts
@@ -0,0 +1,12 @@
+
+
+export interface IAddress {
+    name:        string;
+    lastName:    string;
+    address:     string;
+    address_2?:  string;
+    zip:         string;
+    country:     string;
+    city:        string;
+    phone:       string;
+}
