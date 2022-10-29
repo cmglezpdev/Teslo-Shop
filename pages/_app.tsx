dcbe9e6e73a1e58424ae83d19c15dac5771c0737
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-10-29 00:03:06 -0400

    feat: add provider in the _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app'
 import { ThemeProvider, CssBaseline } from '@mui/material'
+import { SessionProvider } from 'next-auth/react';
 import { SWRConfig } from  'swr';
 import { lightTheme } from '../themes';
 import { UIProvider, CartProvider, AuthProvider } from '../context';
@@ -8,22 +9,24 @@ import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <SWRConfig value={{
-        fetcher: (...args:[key: string]) => fetch(...args).then(res => res.json())
-      }}
-    >
-      <AuthProvider>
-        <CartProvider>
-          <UIProvider>
-            <ThemeProvider theme={lightTheme}>
-              <CssBaseline>
-                <Component {...pageProps} />
-              </CssBaseline>
-            </ThemeProvider>
-          </UIProvider>
-        </CartProvider>
-      </AuthProvider>
-    </SWRConfig>
+    <SessionProvider>
+      <SWRConfig value={{
+          fetcher: (...args:[key: string]) => fetch(...args).then(res => res.json())
+        }}
+      >
+        <AuthProvider>
+          <CartProvider>
+            <UIProvider>
+              <ThemeProvider theme={lightTheme}>
+                <CssBaseline>
+                  <Component {...pageProps} />
+                </CssBaseline>
+              </ThemeProvider>
+            </UIProvider>
+          </CartProvider>
+        </AuthProvider>
+      </SWRConfig>
+    </SessionProvider>
   )
 }
 
