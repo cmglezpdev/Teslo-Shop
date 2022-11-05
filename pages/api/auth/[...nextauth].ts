d8c3bafc1b709ae64d353e53d765bcd32b287a87
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-11-04 21:00:45 -0400

    feat: add login for credentials in next auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,15 +1,60 @@
-import NextAuth from "next-auth"
+import NextAuth, { User } from "next-auth"
+import Credentials from "next-auth/providers/credentials"
 import GithubProvider from "next-auth/providers/github"
 
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
+
+    Credentials({
+      name: 'Custom Login',
+      credentials: {
+        email: { label: 'Email', type: 'email', placeholder: 'email@email.com' },
+        password: { label: 'Password', type: 'password', placeholder: 'Password' },
+      },
+
+      async authorize( credentials, req ) {
+        console.log(credentials)
+        // const { email, password } = credentials as { email: string, password: string };
+        return { email: 'correo@goeew.com', name: 'Carlos M', role: 'admin' } as unknown as User;
+      }
+    }),
+
     GithubProvider({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
     }),
     // ...add more providers here
   ],
+
+  // Callbacks
+  callbacks: {
+    async jwt({ token, account, user }:any) {
+      
+      if( account ) {
+        token.accessToken = account.access_token;
+      
+        switch( account.type ) {
+          case 'oauth':
+            // TODO: crear usuario o verificar si existe en la db
+            break;
+
+          case 'credentials':
+            token.user = user;
+            break;
+        }
+      }
+
+      return token;
+    },
+
+    async session({ session, token, user }:any) {
+      session.accessToken = token.accessToken;
+      session.user = token.user as any;
+
+      return session;
+    }
+  }
 }
 
 export default NextAuth(authOptions)
