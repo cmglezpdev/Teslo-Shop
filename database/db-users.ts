1fec4f2f1c3affa8e71ffc45be69c84dadebb342
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-11-07 20:01:28 -0500

    feat:get user or create a new using login with providers

--- database/db-users.ts
+++ database/db-users.ts
@@ -20,3 +20,26 @@ export const checkUserEmailPassword = async( email: string, password: string ) =
         role
     }
 }
+
+export const oAuthToDbUser = async( oAuthEmail: string, oAuthName: string ) => {
+    await db.connect();
+    const user = await User.findOne({ email: oAuthEmail });
+
+    if( user ) {
+        await db.disconnect();
+        const { _id, name, email, role } = user;
+        return { _id, name, email, role };
+    }
+
+    const newUser = new User({ 
+        email: oAuthEmail, 
+        name: oAuthName, 
+        password: '@',
+        role: 'client',
+    })
+    await newUser.save();
+    await db.disconnect();
+
+    const { _id, name, email, role } = newUser;
+    return { _id, name, email, role };
+}
