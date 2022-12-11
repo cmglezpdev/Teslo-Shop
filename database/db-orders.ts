e1c087162814a967678df9c5cc7532a6ec9359a0
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-12-10 21:23:26 -0500

    feat: get all order from a user

--- database/db-orders.ts
+++ database/db-orders.ts
@@ -19,3 +19,14 @@ export const getOrderById = async (id: string): Promise<IOrder | null> => {
 
     return JSON.parse(JSON.stringify(order));
 }
+
+export const getOrdersFromUser = async (userId:string): Promise<IOrder[] | null> => {
+    if( !isValidObjectId(userId) )
+        return null;
+
+    await db.connect();
+    const orders = await Order.find({ user: userId }).lean();
+    await db.disconnect();
+
+    return JSON.parse(JSON.stringify(orders));
+}
