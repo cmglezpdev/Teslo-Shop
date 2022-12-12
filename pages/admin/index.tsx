774c8860c673e926755a2e1197d25d79e811dda2
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-12-11 20:59:09 -0500

    feat: show real statistics

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,55 +1,104 @@
+import { useState, useEffect } from 'react';
 import { NextPage } from 'next';
-import { Grid } from '@mui/material';
-import { AccessTimeOutlined, AttachMoneyOutlined, CancelPresentationOutlined, CategoryOutlined, CreditCardOffOutlined, CreditCardOutlined, DashboardOutlined, GroupOutlined, ProductionQuantityLimitsOutlined } from '@mui/icons-material';
+import { Grid, Typography } from '@mui/material';
+import useSWR from 'swr';
 import { AdminLayout } from '../../layouts/AdminLayout';
 import { SummaryTile } from '../../components/admin';
+import { DashboardSummaryResponse } from '../../interfaces';
+
+import { 
+    AccessTimeOutlined, 
+    AttachMoneyOutlined, 
+    CancelPresentationOutlined, 
+    CategoryOutlined, 
+    CreditCardOffOutlined, 
+    CreditCardOutlined, 
+    DashboardOutlined, 
+    GroupOutlined, 
+    ProductionQuantityLimitsOutlined 
+} from '@mui/icons-material';
+
 
 const DashboardPage:NextPage = () => {
+    
+    const { data, error } = useSWR<DashboardSummaryResponse>('/api/admin/dashboard', { 
+        refreshInterval: 30000  // 30 seconds
+    });
+
+    const [refreshIn, setRefreshIn] = useState(30);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setRefreshIn((prev) => (prev - 1 + 31)%31);
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, [])
+
+    
+    if( !error && !data ) {
+        return <></>
+    }
+
+    if( error ) {
+        console.log(error);
+        return <Typography>Error to load the information</Typography>
+    }
+
+    const { 
+        paidOrders,
+        notPaidOrders,
+        numberOfOrders,
+        numberOfClients,
+        numberOfProducts,
+        productsWithNoInventary,
+        lowInventary
+     } = data!;
+
     return (
         <AdminLayout
             title='Dashboard'
             subtitle='General Statistics'
             icon={ <DashboardOutlined /> }
         >
-
             <Grid container spacing={2}>
                 <SummaryTile 
-                    title={ 1 } 
+                    title={ numberOfOrders } 
                     subTitle="Total Orders" 
                     icon={ <CreditCardOutlined color='secondary' sx={{fontSize: 40}} />} 
                 />
                 <SummaryTile 
-                    title={ 2 } 
+                    title={ paidOrders } 
                     subTitle="Paid Orders" 
                     icon={<AttachMoneyOutlined color='secondary' sx={{fontSize: 40}} />} 
                 />
                 <SummaryTile 
-                    title={ 2 } 
+                    title={ notPaidOrders } 
                     subTitle="Pending Orders" 
                     icon={<CreditCardOffOutlined color='error' sx={{fontSize: 40}} />} 
                 />
                 <SummaryTile 
-                    title={ 3 }
+                    title={ numberOfClients }
                     subTitle="Clients" 
                     icon={<GroupOutlined color='secondary' sx={{fontSize: 40}} />} 
                 />
                 <SummaryTile 
-                    title={ 20 } 
+                    title={ numberOfProducts } 
                     subTitle="Products" 
                     icon={<CategoryOutlined color='secondary' sx={{fontSize: 40}} />} 
                 />
                 <SummaryTile 
-                    title={ 4 } 
+                    title={ productsWithNoInventary } 
                     subTitle="Out of Stock" 
                     icon={<CancelPresentationOutlined color='error' sx={{fontSize: 40}} />} 
                 />
                 <SummaryTile 
-                    title={ 4 } 
+                    title={ lowInventary } 
                     subTitle="Low Inventary" 
                     icon={<ProductionQuantityLimitsOutlined color='warning' sx={{fontSize: 40}} />} 
                 />
                 <SummaryTile 
-                    title={ 8 } 
+                    title={ refreshIn } 
                     subTitle="Loading in: " 
                     icon={<AccessTimeOutlined color='secondary' sx={{fontSize: 40}} />} 
                 />
