41eeb6d2751b6741ab102d1fb447971cf22dec20
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-12-13 14:06:15 -0500

    feat: add task to middleware

--- middleware.ts
+++ middleware.ts
@@ -4,22 +4,30 @@ import { NextRequest, NextResponse } from 'next/server';
 // import { jwt } from './services';
 
 export async function middleware ( req:NextRequest ) {
+    
     const token = req.cookies.get('token') || '';
     try {
-
         // TODO: Temporal solution. remplace by 
         // await jwt.isValidToken( token );
         if( token.length <= 10 )
             throw new Error(); 
-
-        // await jwt.isValidToken(token)
-        return NextResponse.next();
     } catch (error) {
         return NextResponse.redirect(new URL( `/auth/login?p=${req.nextUrl.pathname}`, req.nextUrl.origin ))
     }
 
+    // TODO: Validar que el usuario tenga el rol necesario para acceder a la ruta
+    // const validRoles = ['admin', 'super-admin', 'SEO'];
+    // if( req.nextUrl.pathname === '/admin') {
+    //     if( !validRoles.includes( useRole ) )
+    //         return NextResponse.redirect(new URL( `/`, req.nextUrl.origin ))
+    // }
+    
+    return NextResponse.next();
+
+    // TODO: Validar las rutas del  admin endpoint (VIDEO 10, Seccion 21)
+
 }
 
 export const config = {
-    matcher: ['/checkout/:path']
+    matcher: ['/checkout/:path', '/admin']
 }
