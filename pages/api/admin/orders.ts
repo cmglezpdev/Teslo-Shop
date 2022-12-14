2d93a17e3a7ae0b4aeb138771218050ac2a3f763
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-12-13 21:28:12 -0500

    feat: add endpoint to get all orders

---
+++ pages/api/admin/orders.ts
@@ -0,0 +1,33 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { db } from '../../../database';
+import { Order } from '../../../models';
+import { IOrder } from '../../../interfaces';
+
+type Data = 
+    | { message: string }
+    | IOrder[]
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+    
+    switch( req.method ) {
+        case 'GET':
+            return getOrders(req, res);
+
+        default:
+            return res.status(400).json({ message: 'Bad request' })
+    }
+    
+}   
+
+async function getOrders(req: NextApiRequest, res: NextApiResponse<Data>) {
+
+    await db.connect();
+
+    const orders = await Order.find()
+        .sort({ createdAt: 'desc' })
+        .populate('user', 'name email')
+        .lean();
+    await db.disconnect();
+
+    return res.status(200).json(orders);
+}
