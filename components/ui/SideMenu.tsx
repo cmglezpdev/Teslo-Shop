d7f9b265d5e23467f356a65d2aea51fa6b0103a2
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-12-13 21:28:33 -0500

    feat: add link to order pages

--- components/ui/SideMenu.tsx
+++ components/ui/SideMenu.tsx
@@ -140,7 +140,7 @@ export const SideMenu = () => {
                             <ListItemText primary='Products' />
                         </ListItem>
     
-                        <ListItem button>
+                        <ListItem button onClick={ () => navigateTo('/admin/orders') }>
                             <ListItemIcon>
                                 <ConfirmationNumberOutlined/>
                             </ListItemIcon>
