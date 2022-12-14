14b57d7dacae8f5edfa67bdab716d127b2c3f020
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-12-13 21:27:32 -0500

    fix: add white space between icon and title

--- layouts/AdminLayout.tsx
+++ layouts/AdminLayout.tsx
@@ -28,6 +28,7 @@ export const AdminLayout:FC<Props> = ({ children, title, subtitle, icon }) => {
                 <Box display='flex' flexDirection='column'>
                     <Typography variant='h1' component='h1'>
                         { icon }
+                        {' '}
                         { title }
                     </Typography>
                     <Typography variant='h2' sx={{ mb: 1 }}>
