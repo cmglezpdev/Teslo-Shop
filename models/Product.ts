2cef4d092a75cf0a29d729d832cf85d1c4e51522
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-12-22 23:45:33 -0500

    fix: add default values

--- models/Product.ts
+++ models/Product.ts
@@ -2,10 +2,10 @@ import mongoose, { Model, model, Schema } from 'mongoose';
 import { IProduct } from '../interfaces';
 
 const productSchrema = new Schema({
-    description: { type: String, require: true },
+    description: { type: String, required: true, default: '' },
     images: [{ type: String }],
-    inStock: { type: Number, require: true, default: 0 },
-    price: { type: Number, require: true, default: 0 },
+    inStock: { type: Number, required: true, default: 0 },
+    price: { type: Number, required: true, default: 0 },
     sizes: [{
         type: String,
         enum: {
@@ -13,22 +13,24 @@ const productSchrema = new Schema({
             message: '{VALUE} is not a valid size'
         }
     }],
-    slug: { type: String, require: true, unique: true },
+    slug: { type: String, required: true, unique: true },
     tags: [{ type: String }],
-    title: { type: String, require: true },
+    title: { type: String, required: true, default: '' },
     type: {
         type: String,
         enum: {
             values: ['shirts', 'pants', 'hoodies', 'hats'],
             message: '{VALUE} is not a valid type'
-        }
+        },
+        default: 'shirts'
     },
     gender: {
         type: String,
         enum: {
             values: ['men', 'women', 'kid', 'unisex'],
             message: '{VALUE} is not a gender type'
-        }
+        },
+        default: 'women'
     },
 },{
     timestamps: true
