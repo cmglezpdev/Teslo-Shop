7bd0c32ec92da7d490a35757c2745805f909b8a3
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-12-22 23:36:32 -0500

    feat: add update product endpoint

--- pages/api/admin/products.ts
+++ pages/api/admin/products.ts
@@ -1,11 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { isValidObjectId } from 'mongoose';
 import { db } from '../../../database';
-import { IProduct } from '../../../interfaces/products';
-import Product from '../../../models/Product';
+import { Product } from '../../../models';
+import { IProduct } from '../../../interfaces';
 
 type Data = 
     | { message: string }
     | IProduct[]
+    | IProduct
 
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
@@ -15,6 +17,8 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<Data>)
             return getProducts(req, res);
 
         case 'PUT':
+            return updateProduct(req, res);
+
         case 'POST':
 
         default:
@@ -36,3 +40,37 @@ async function getProducts(req: NextApiRequest, res: NextApiResponse<Data>) {
 
     res.status(200).json(products);
 }  
+
+async function updateProduct(req: NextApiRequest, res: NextApiResponse<Data>) {
+    
+    const { _id = '', images = [] } = req.body as IProduct;
+    
+    console.log(_id);
+
+    if( !isValidObjectId(_id) )
+        return res.status(400).json({ message: 'Id Product is not valid' });
+    
+    if( images.length < 2 ) 
+        return res.status(400).json({ message: 'At least two images are required' });
+        
+    // TODO: Tendremos localhost:3000//products/image.jpg
+    
+    try {
+        await db.connect();
+        const product = await Product.findById(_id);
+        if(!product)  
+            return res.status(400).json({ message: 'The product does not exist' });
+            
+        // TODO: Delete images from cloudinary
+        
+        await product.update(req.body);
+        // await product.save();
+        await db.disconnect();
+        
+        return res.status(200).json( product );
+    } catch (error) {
+        await db.disconnect();
+        return res.status(400).json({ message: 'See server logs' });  
+    }
+}
+
